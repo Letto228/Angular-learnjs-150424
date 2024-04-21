385d19e1cfb8636921c921c137942c580f21a057
glazirv <glazirv@gmail.com>
2024-04-21 22:32:21 +0300

    homework-1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,8 +26,6 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain(
-            'Angular-learnjs-150424 app is running!',
-        );
+        expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-learnjs-150424 app is running!');
     });
 });
