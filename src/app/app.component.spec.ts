773b478e0439bfe24b55957d9e0f8c6f51348557
Stepkin Aleksandr Yurevich <stepkin@speechpro.com>
2024-04-24 12:08:26 +0300

    feat: добавлен eslint-конфиг airbnb

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    beforeEach(() => TestBed.configureTestingModule({
-        imports: [RouterTestingModule],
-        declarations: [AppComponent],
-    }));
+    beforeEach(() =>
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            declarations: [AppComponent],
+        }),
+    );
 
     it('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
@@ -14,7 +16,7 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it('should have as title \'Angular-learnjs-150424\'', () => {
+    it("should have as title 'Angular-learnjs-150424'", () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app.title).toEqual('Angular-learnjs-150424');
