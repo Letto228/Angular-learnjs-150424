0d58e308db1a9a460f48d383aa7c60ee2dcd2ec9
Boss <boss@Bosss-MacBook-Pro.local>
2024-04-25 14:39:06 +0200

    feat: add card component

--- src/app/pages/products-list/card/card/card.module.ts
+++ src/app/pages/products-list/card/card/card.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {CardComponent} from './card.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     declarations: [CardComponent],
-    imports: [CommonModule],
+    exports: [CardComponent],
+    imports: [MatCardModule, MatButtonModule],
 })
 export class CardModule {}
