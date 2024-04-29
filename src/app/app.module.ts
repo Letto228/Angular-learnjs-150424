3c7d168f765cd052d9b2884eb8ad20d8d15380f4
Руслан <Mediumgr@yandex.ru>
2024-04-29 20:11:04 +0300

    feat: changes after review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderModule} from './components/header/header.module';
-import {CardModule} from './pages/products-list/card/card.module';
 import {ProductsListModule} from './pages/products-list/products-list.module';
 
 @NgModule({
     declarations: [AppComponent],
     exports: [],
     imports: [
-        CardModule,
         HeaderModule,
         BrowserModule,
         AppRoutingModule,
