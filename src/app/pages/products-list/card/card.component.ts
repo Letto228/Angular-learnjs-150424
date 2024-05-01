c0b0dd853b61a1b7d19708288144088ba6e0a542
= <=>
2024-05-01 21:32:02 +0300

    fix: fix propagation on card, class binding style, product initial state

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -7,10 +7,14 @@ import {Product} from 'src/app/shared/products/product.interface';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    @Input() product!: Product;
-    @Output() buy = new EventEmitter<string>();
+    @Input() product: Product | null = null;
+    @Output() readonly buy = new EventEmitter<string>();
 
-    buyProduct(product: Product) {
-        this.buy.emit(product._id);
+    buyProduct(event: MouseEvent, product: Product | null) {
+        event.stopPropagation();
+
+        if (product) {
+            this.buy.emit(product._id);
+        }
     }
 }
