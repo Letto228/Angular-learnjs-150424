c4b0d80f7e77bb8eba591767ec269864ef77f6b8
Igor Chernyavsky <ic8550@gmail.com>
2024-05-02 03:33:10 +0300

    fix: remove irrelevant piece of code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import {applicationConfigMock} from './shared/application-config/application-con
 export class AppComponent {
     readonly aplicationConfigMock = applicationConfigMock;
     isSidenavOpenedStore = false;
-    productId!: string;
     onMenuClick(_event: Event) {
         this.isSidenavOpenedStore = !this.isSidenavOpenedStore;
     }
