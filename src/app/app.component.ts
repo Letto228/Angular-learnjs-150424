a3927eae60504c4179cd17e94a8d19c7ad3eb54a
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-05-03 21:22:59 +0300

    feat: add lecture 4 homework stand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,25 @@ import {applicationConfigMock} from './shared/application-config/application-con
 })
 export class AppComponent {
     readonly aplicationConfigMock = applicationConfigMock;
+
+    switchTemplate = false;
+    closeTemplate = true;
+
+    constructor() {
+        setTimeout(() => {
+            this.switchTemplate = !this.switchTemplate;
+            // or
+            this.closeTemplate = !this.closeTemplate;
+        }, 3000);
+        setTimeout(() => {
+            this.switchTemplate = !this.switchTemplate;
+            // or
+            this.closeTemplate = !this.closeTemplate;
+        }, 6000);
+        setTimeout(() => {
+            this.switchTemplate = !this.switchTemplate;
+            // or
+            this.closeTemplate = !this.closeTemplate;
+        }, 9000);
+    }
 }
