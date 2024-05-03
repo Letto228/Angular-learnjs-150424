231a2135105a786e1929e1f0429e19281e1805fc
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-05-03 21:21:27 +0300

    chore: sanitize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component} from '@angular/core';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    encapsulation: ViewEncapsulation.Emulated,
 })
 export class AppComponent {
     readonly aplicationConfigMock = applicationConfigMock;
-
-    // isSidenavOpenedStore = false;
-
-    // onMenuClick(_event: Event) {
-    //     this.isSidenavOpenedStore = !this.isSidenavOpenedStore;
-    // }
 }
