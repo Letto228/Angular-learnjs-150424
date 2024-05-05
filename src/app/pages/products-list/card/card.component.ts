97d7b08b56ab94826debcd1232d6e866b6a80ec0
Boss <boss@Bosss-MacBook-Pro.local>
2024-05-05 13:23:40 +0200

    feat: add solution dynamic card component

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,8 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+    @Input() product: Product | null = null;
+
+    @Output() buyButtonClick = new EventEmitter<string | undefined>();
+
+    getImage(): string | undefined {
+        return this.product?.images[0].url;
+    }
+
+    getIdOnClick(event: MouseEvent) {
+        event.stopPropagation();
+        this.buyButtonClick.emit(this.product?._id);
+    }
+}
