14b7b38d9525a93be1401bac0954e75f5dd365d6
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-05-07 00:00:27 +0300

    chore: sanitize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,12 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    // changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-    private counter = 0;
-
     readonly aplicationConfigMock = applicationConfigMock;
-
-    switchTemplate = false;
-    closeTemplate = true;
-
-    constructor() {
-        setTimeout(() => {
-            // eslint-disable-next-line no-console
-            console.log('Set timeout');
-            this.switchTemplate = !this.switchTemplate;
-            // or
-            this.closeTemplate = !this.closeTemplate;
-        }, 3000);
-        setTimeout(() => {
-            // eslint-disable-next-line no-console
-            console.log('Set timeout');
-            this.switchTemplate = !this.switchTemplate;
-            // or
-            this.closeTemplate = !this.closeTemplate;
-        }, 6000);
-        setTimeout(() => {
-            // eslint-disable-next-line no-console
-            console.log('Set timeout');
-            this.switchTemplate = !this.switchTemplate;
-            // or
-            this.closeTemplate = !this.closeTemplate;
-        }, 9000);
-    }
-
-    onKeyDown() {}
-
-    getCount() {
-        setTimeout(() => {
-            this.counter += 1;
-        });
-
-        return this.counter;
-    }
-
-    get title(): string {
-        // eslint-disable-next-line no-console
-        console.log('Run CD');
-
-        return this.aplicationConfigMock.title;
-    }
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -2,10 +2,7 @@ import {
     ChangeDetectionStrategy,
     ChangeDetectorRef,
     Component,
-    ContentChild,
-    TemplateRef,
     ViewChild,
-    ViewContainerRef,
     inject,
 } from '@angular/core';
 import {MatDrawer} from '@angular/material/sidenav';
@@ -19,32 +16,10 @@ import {MatDrawer} from '@angular/material/sidenav';
 export class SidenavComponent {
     @ViewChild(MatDrawer, {static: true}) private readonly drawerComponent: MatDrawer | undefined;
 
-    @ViewChild('viewport', {read: ViewContainerRef, static: true})
-    private readonly viewport: ViewContainerRef | undefined;
-
-    @ContentChild('navigationTemplate', {static: true})
-    private readonly navigationTemplate: TemplateRef<unknown> | undefined;
-
     private readonly changeDetectorRef = inject(ChangeDetectorRef);
-
-    selectChildrenView = 'mat-list';
-
-    // constructor(private readonly changeDetectorRef: ChangeDetectorRef) {
-    constructor() {
-        setTimeout(() => {
-            if (this.navigationTemplate) {
-                this.viewport?.createEmbeddedView(this.navigationTemplate);
-            }
-        }, 100);
-    }
 
     toggleSidenavOpened() {
         this.drawerComponent?.toggle();
         this.changeDetectorRef.markForCheck();
-    }
-
-    insertNavigationTemplate(templateRef: TemplateRef<unknown>) {
-        this.viewport?.clear();
-        this.viewport?.createEmbeddedView(templateRef);
     }
 }
