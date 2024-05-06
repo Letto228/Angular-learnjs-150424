58a748c6e52c8b8ac91a22023ac1ee563bb02f1b
Aleksandr Mikhailov <zzamixzz@yandex.ru>
2024-05-06 19:40:55 +0300

    feat: add popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {AppComponent} from './app.component';
 import {HeaderModule} from './components/header/header.module';
 import {ProductsListModule} from './pages/products-list/products-list.module';
 import {SidenavModule} from './components/sidenav/sidenav.module';
+import {PopupHostComponent} from './pages/popup-host/popup-host.component';
 
 @NgModule({
     declarations: [AppComponent],
@@ -20,6 +21,7 @@ import {SidenavModule} from './components/sidenav/sidenav.module';
         ProductsListModule,
         SidenavModule,
         MatListModule,
+        PopupHostComponent,
     ],
     bootstrap: [AppComponent],
 })
---
+++ src/app/pages/popup-host/popup-host.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+    selector: 'app-popup-host',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './popup-host.component.html',
+    styleUrls: ['./popup-host.component.css'],
+})
+export class PopupHostComponent {
+    @ViewChild('viewport', {read: ViewContainerRef, static: true})
+    private readonly viewport: ViewContainerRef | undefined;
+
+    @Input() set template(tmpl: TemplateRef<any> | null) {
+        this.viewport?.clear();
+
+        if (tmpl) {
+            this.isHidden = false;
+            this.viewport?.createEmbeddedView(tmpl);
+        } else {
+            this.isHidden = true;
+        }
+    }
+
+    isHidden = true;
+}
