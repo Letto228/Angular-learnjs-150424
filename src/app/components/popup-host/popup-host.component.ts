5f9624cbdfeaceee128e453152b6ffaa8a4843c4
kimkuular <53006124+kimkuular@users.noreply.github.com>
2024-05-06 23:41:24 +0700

    refactor:  template Input changes via setter

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -19,37 +19,15 @@ export class PopupHostComponent {
     @ViewChild('vc', {read: ViewContainerRef})
     private readonly vc: ViewContainerRef | undefined;
 
-    @Input() template: TemplateRef<unknown> | null = null;
-
-    constructor() {
-        setTimeout(() => {
-            if (this.template) {
-                this.openDialog(this.template);
-            } else {
-                this.dialog?.nativeElement.close();
-            }
-        }, 3000);
-
-        setTimeout(() => {
-            if (this.template) {
-                this.openDialog(this.template);
-            } else {
-                this.dialog?.nativeElement.close();
-            }
-        }, 6000);
-
-        setTimeout(() => {
-            if (this.template) {
-                this.openDialog(this.template);
-            } else {
-                this.dialog?.nativeElement.close();
-            }
-        }, 9000);
-    }
-
-    private openDialog(template: TemplateRef<unknown>): void {
-        this.vc?.clear();
-        this.vc?.createEmbeddedView(template);
-        this.dialog?.nativeElement.showModal();
+    @Input()
+    set template(tmpl: TemplateRef<unknown> | null) {
+        if (tmpl) {
+            this.vc?.clear();
+            this.vc?.createEmbeddedView(tmpl);
+            this.dialog?.nativeElement.showModal();
+        } else {
+            this.vc?.clear();
+            this.dialog?.nativeElement.close();
+        }
     }
 }
