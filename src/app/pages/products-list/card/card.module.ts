510db73be4e39af2d0ca3d0df95ef8f9d3957896
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-05-14 00:47:45 +0300

    chore: sanitize project

--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -4,13 +4,10 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {CardComponent} from './card.component';
-// import {CurrencyModule} from '../../../shared/currency/currency.module';
-// import { CurrencyPipe } from '../../../shared/currency/currency.pipe';
 
 @NgModule({
     declarations: [CardComponent],
     imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
-    // providers: [CurrencyPipe],
     exports: [CardComponent],
 })
 export class CardModule {}
