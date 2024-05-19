09e526a0bd499400bb83edf4ae449e38e8704db7
Alexander Sobko <45943974+neyriks@users.noreply.github.com>
2024-05-19 21:22:18 +0300

    Removed unnecessary code

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -7,15 +7,4 @@ import {Component, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/
 })
 export class PopupHostComponent implements OnInit {
     @Input() template: TemplateRef<any> | null = null;
-
-    constructor(private readonly viewContainerRef: ViewContainerRef) {}
-
-    ngOnInit(): void {
-        if (this.template) {
-            this.viewContainerRef.clear();
-            this.viewContainerRef.createEmbeddedView(this.template);
-        } else {
-            this.viewContainerRef.clear();
-        }
-    }
 }
