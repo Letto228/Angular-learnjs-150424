5fe4a878f166c9e6ced126159b297edfb03f05cd
Igor Chernyavsky <ic8550@gmail.com>
2024-05-20 15:31:32 +0300

    refactor: reduce the number of <popup-host> elements from 3 to 1, but keep {static:false}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, TemplateRef, ViewChild} from '@angular/core';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
 
 @Component({
@@ -7,8 +7,27 @@ import {applicationConfigMock} from './shared/application-config/application-con
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+    @ViewChild('dialog1', {static: true}) dialog1Template: TemplateRef<unknown> | null = null;
+    @ViewChild('dialog2', {static: true}) dialog2Template: TemplateRef<unknown> | null = null;
+    @ViewChild('dialog3', {static: true}) dialog3Template: TemplateRef<unknown> | null = null;
     readonly aplicationConfigMock = applicationConfigMock;
     shouldShowDialog1 = false;
     shouldShowDialog2 = false;
     shouldShowDialog3 = false;
+
+    getDialogToShow() {
+        if (this.shouldShowDialog1) {
+            return this.dialog1Template;
+        }
+
+        if (this.shouldShowDialog2) {
+            return this.dialog2Template;
+        }
+
+        if (this.shouldShowDialog3) {
+            return this.dialog3Template;
+        }
+
+        return null;
+    }
 }
