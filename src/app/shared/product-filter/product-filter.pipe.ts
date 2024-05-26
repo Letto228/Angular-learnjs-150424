1d4a2d6844bc487e3a43b459b065827af0f6d258
Boss <boss@Bosss-MacBook-Pro.local>
2024-05-26 23:44:07 +0200

    feat: change pipe logic

--- src/app/shared/product-filter/product-filter.pipe.ts
+++ src/app/shared/product-filter/product-filter.pipe.ts
@@ -5,12 +5,30 @@ import {Product} from '../products/product.interface';
     name: 'productFilter',
 })
 export class ProductFilterPipe implements PipeTransform {
-    transform(products: Product[] | null, name: string): Product | null {
+    transform(products: Product[] | null, name: string): Product[] | null {
         if (!products) {
             return null;
         }
 
-        return products.find(product => product.name.includes(name)) || null;
+        if (!name) {
+            return products;
+        }
+
+        return products.filter(product => product.name.includes(name));
+
+        // для себя
+        // let filteredProducts: Product[] = [];
+        // for (let i = 0; i <= products!.length - 1; i++) {
+        //     if (products[i].name.includes(name)) {
+        //         filteredProducts.push(products[i]);
+        //     }
+        // }
+        // return filteredProducts;
+        // if (!products) {
+        //     return null;
+        // }
+
+        // return products.find(product => product.name.includes(name)) || null;
 
         // for (let product of products) {
         //     if (product.name.includes(name)) {
