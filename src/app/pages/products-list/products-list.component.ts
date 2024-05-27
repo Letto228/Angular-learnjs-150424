46e40577cef94835751ff0b4f561c818a4b10c7e
Aleksandr Mikhailov <zzamixzz@yandex.ru>
2024-05-27 14:49:22 +0300

    fix: remove no lint

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -8,13 +8,12 @@ import {productsMock} from '../../shared/products/products.mock';
 })
 export class ProductsListComponent {
     readonly productsMock = productsMock;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
     cardClick(id: string) {
         // eslint-disable-next-line no-console
         console.log(`${id} card click`);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     buyClick(id: string) {
         // eslint-disable-next-line no-console
         console.log(`${id} buy click`);
