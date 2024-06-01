4cb2bca7e64aa44c2acc5a5ad6985485877fbba2
= <=>
2024-06-01 15:27:46 +0300

    fix: fix scroll direction

--- src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -7,19 +7,25 @@ import {LoadDirection} from './scroll-with-loading.interface';
 })
 export class ScrollWithLoadingDirective {
     private readonly borderOffset = 100;
+    private lastOffsetTop = 0;
     @Output() loadData = new EventEmitter<LoadDirection>();
     loadDirection: LoadDirection | null = null;
 
     @HostListener('scroll', ['$event.target'])
     onScroll(element: Element) {
         const {scrollTop: offsetTop, scrollHeight, clientHeight} = element;
+        const lastOffsetTop = this.lastOffsetTop;
+
+        this.lastOffsetTop = offsetTop;
         const offsetBottom = scrollHeight - offsetTop - clientHeight;
 
-        if (offsetTop <= this.borderOffset) {
+        this.loadDirection = offsetTop > lastOffsetTop ? LoadDirection.DOWN : LoadDirection.UP;
+
+        if (offsetTop <= this.borderOffset && this.loadDirection === LoadDirection.UP) {
             this.loadData.emit(LoadDirection.UP);
         }
 
-        if (offsetBottom <= this.borderOffset) {
+        if (offsetBottom <= this.borderOffset && this.loadDirection === LoadDirection.DOWN) {
             this.loadData.emit(LoadDirection.DOWN);
         }
     }
